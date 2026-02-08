951604efcb68c1231ee2f0332b413f392993bce3
Hasan Ulu <71779845+hasanulu@users.noreply.github.com>
2026-02-09 02:32:04 +0300

    Fix: selam

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Merhaba DevOps Dünyasi!</h1><p>Bu uygulama Docker üzerinde çalişigit add .yor.</p>'
+    return '<h1>Merhaba DevOps Dünyasi!</h1><p>Ben geldim herkes ayağini denk alsin.</p>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
